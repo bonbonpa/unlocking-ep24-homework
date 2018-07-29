2b140b8629565bd404afffe7de90631bad21cf0e
bonbonpa <darksphin@gmail.com>
2018-07-29 23:53:51 +0700

    implement homework

--- ep24.web/Controllers/OrderController.cs
+++ ep24.web/Controllers/OrderController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -24,20 +24,23 @@ namespace ep24.web.Controllers
         [HttpGet]
         public IEnumerable<Order> ListHistory()
         {
-             //TODO: implement scenario ขอรายการสั่งซื้อที่ยืนยันรายการสั่งซื้อแล้ว
-
-            throw new NotImplementedException();
+            //TODO: implement scenario ขอรายการสั่งซื้อที่ยืนยันรายการสั่งซื้อแล้ว
+            return orderRepo.List(o => o.PaidDate.HasValue);
         }
 
         [HttpPost]
         public OrderProductResponse OrderProduct([FromBody]OrderProductRequest request)
         {
             //TODO: implement scenario ไม่มีข้อมูล หรือไม่เลือกสินค้าที่จะสั่ง ให้แจ้งกลับว่า 'ไม่พบเมนูที่จะสั่ง' และไม่บันทึกข้อมูล   
+            if (request == null || request.OrderedProducts == null || !request.OrderedProducts.Any())
+            {
+                return new OrderProductResponse { Message = "ไม่พบเมนูที่จะสั่ง" };
+            }
 
-            var productIds = request.OrderedProducts.Select(p => p.Key);            
+            var productIds = request.OrderedProducts.Select(p => p.Key);
             var products = productRepo.GetAllProducts();
             var filteredProducts = products.Where(p => productIds.Contains(p.Id)).ToList();
-        
+
             if (filteredProducts.Count() != productIds.Count())
             {
                 return new OrderProductResponse { Message = "ไม่พบสินค้าบางรายการ กรุณาสั่งใหม่อีกครั้ง", };
@@ -57,7 +60,8 @@ namespace ep24.web.Controllers
                 Username = request.Username,
             };
             orderRepo.Create(order);
-            
+
+            // TODO: 
             return new OrderProductResponse { Message = "สั่งเมนูสำเร็จ กรุณาชำระเงินที่เค้าเตอร์", ReferenceCode = order.ReferenceCode, };
         }
     }
